e59c598a57b6045f048cfc4a462f10730fc9fc9a
unknown <smayer04@CSPC060.dcs.bbk.ac.uk>
2014-11-03 17:54:46 +0000

    Added file 'calculator.java' and empty methods.

---
+++ calculator.java
@@ -0,0 +1,21 @@
+class Calculator {
+	public int add(int x, int y) {
+		return 0;
+	}
+	
+	public int subtract(int x, int y) {
+		return 0;
+	}
+	
+	public int multiply(int x, int y) {
+		return 0;
+	}
+	
+	public double divide(int x, int y) {
+		return 0;
+	}
+	
+	public int modulus(int x, int y) {
+		return 0;
+	}
+}
