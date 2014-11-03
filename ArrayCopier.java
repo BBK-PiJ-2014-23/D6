7380b142dfbe293450b9cabc7e33e028dd0c8dcd
unknown <smayer04@CSPC060.dcs.bbk.ac.uk>
2014-11-03 20:54:17 +0000

    Fixed logic of class ArrayCopier.

--- ArrayCopier.java
+++ ArrayCopier.java
@@ -2,10 +2,9 @@ public class ArrayCopier {
 	
 	public void copy(int[] src, int[] dst) {
 		for (int i = 0; i < dst.length; i++) {
-			dst[i] = src[i];
-		}
-		if (dst.length > src.length) {
-			for (int i = src.length; i < dst.length; i++) {
+			if (src.length > i) {
+				dst[i] = src[i];
+			} else {
 				dst[i] = 0;
 			}
 		}
