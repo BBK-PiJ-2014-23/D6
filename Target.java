919e0834eb2ef3d6b515c2b8cd2e92142adab82f
unknown <stefanm@LONWRK07474.eidos.com>
2014-11-11 12:58:26 +0000

    Finished class 'Target.java'.

--- Target.java
+++ Target.java
@@ -1,5 +1,6 @@
 public class Target {
-    int[][][] array;
+    private int[][][] array;
+    private int[] target;
 
     public Target(int x, int y, int z) {
         array = new int[x][y][z];
@@ -18,15 +19,30 @@ public class Target {
         int z = (int) Math.abs(array[x][y].length * Math.random());
 
         array[x][y][z] = 1;
+        target = new int[3];
+        target[0] = x;
+        target[1] = y;
+        target[2] = z;
     }
 
     public Result fire (int x, int y, int z) {
         //HIT, FAIL_LEFT, FAIL_RIGHT, FAIL_HIGH, FAIL_LOW, FAIL_SHORT, FAIL_LONG, OUT_OF_RANGE
-        if (array[x][y][z] == 1) {
+        if (x == target[0] && y == target[1] && z == target[2]) {
             return Result.HIT;
-        } else {
+        } else if (x > array.length - 1 || y > array[0].length - 1 || z > array[0][0].length - 1) {
             return Result.OUT_OF_RANGE;
+        } else if (x < target[0]) {
+            return Result.FAIL_LEFT;
+        } else if (x > target[0]) {
+            return Result.FAIL_RIGHT;
+        } else if (y < target[1]) {
+            return Result.FAIL_LOW;
+        } else if (y > target[1]) {
+            return Result.FAIL_HIGH;
+        } else if (z < target[2]) {
+            return Result.FAIL_SHORT;
+        } else {
+            return Result.FAIL_LONG;
         }
     }
 }
-
